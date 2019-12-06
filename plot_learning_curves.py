b5c43d41bddd667cb9bf2690446b7059c2f98323
mcncm <mcnallychristopherm@gmail.com>
2019-12-06 10:35:00 -0500

    Improved learning curve plotting script

--- plot_learning_curves.py
+++ plot_learning_curves.py
@@ -4,15 +4,18 @@ import sys
 
 import numpy as np
 import tensorflow as tf
-# from tensorflow.summary import event_accumulator
 import matplotlib.pyplot as plt
 
 eng_logdir = os.path.join('tacotron2', 'eng-outdir', 'logdir')
 ipa_logdir = os.path.join('tacotron2', 'nostress-ipa-outdir', 'logdir')
 fig_dir = os.path.join('writeup', 'figures')
 
-figsize=(3, 3)
-fontsize=12
+figsize=(6, 4)
+fontsize=16
+
+### THIS IS A MAGIC NUMBER ###
+steps_per_checkpoint = 1000
+##############################
 
 def get_event(dir_path):
     r"""Borrowed from Saki Shinoda
@@ -37,15 +40,67 @@ def get_tag_data(dir_path, tag='validation.loss'):
 
     return np.array(data)
 
-for tag in ['training.loss', 'validation.loss']:
+def smoothed_statistic(statistic, arr, smoothing, starting_point=None):
+    if starting_point == None:
+        starting_point = int(np.ceil(smoothing/2))
+    smoothing_range = int(np.floor(smoothing / 2))
+    return np.array([
+        statistic(arr[i-smoothing_range:i+smoothing_range])
+        for i in range(starting_point,
+                       len(arr) - 1 - int(np.ceil(smoothing / 2)),
+                       smoothing)
+    ])
+
+
+def plot_training_validation(datasets, smoothing=10, starting_point=100):
+    r"""Plot the training and validation loss
+    """
+
     fig, ax = plt.subplots(1, 1, figsize=figsize)
 
-    ax.plot(get_tag_data(eng_logdir, tag), label='English orthography')
-    ax.plot(get_tag_data(ipa_logdir, tag), label='IPA')
+    for i, (dataset_name, logdir) in enumerate(datasets.items()):
+        training_loss = get_tag_data(logdir, 'training.loss')
+        validation_loss = get_tag_data(logdir, 'validation.loss')
 
-    ax.set_xlabel('Training step', fontsize=fontsize)
-    ax.set_ylabel(tag, fontsize=fontsize)
+        training_loss_std = smoothed_statistic(np.std, training_loss, smoothing, starting_point)
+        training_loss_mean = smoothed_statistic(np.mean, training_loss, smoothing, starting_point)
+
+        training_steps = range(starting_point, len(training_loss) - 1, smoothing)
+
+        ax.plot(training_steps, training_loss_mean, color='C{}'.format(i), label=dataset_name)
+        ax.fill_between(training_steps,
+                        training_loss_mean - training_loss_std,
+                        training_loss_mean + training_loss_std,
+                        color='C{}'.format(i), alpha=0.3)
+
+        validation_steps = range(0, steps_per_checkpoint * len(validation_loss), steps_per_checkpoint)
+        # ax.plot(validation_steps, validation_loss, '--', color='C{}'.format(i))
+
+    ax.set_xlim(starting_point, training_steps[-1])
+    ax.set_ylabel('Loss', fontsize=fontsize)
+    ax.set_xlabel('Steps')
     ax.set_yscale('log')
+    ax.grid()
+    ax.legend()
+    print('Saving!')
+    fig.savefig(os.path.join(fig_dir, 'tacotron2_training_ipa.png'))
 
-    fig_filename = 'tacotron2_' + tag.replace('.','_') + '.png'
-    fig.savefig(os.path.join(fig_dir, fig_filename))
+if __name__ == '__main__':
+    datasets = {'English orthography': eng_logdir, 'IPA': ipa_logdir}
+    plot_training_validation(datasets, smoothing=50, starting_point=100)
+
+
+# for tag in ['training.loss', 'validation.loss']:
+# 
+#     ax.plot(get_tag_data(eng_logdir, tag), label='English orthography')
+#     ax.plot(get_tag_data(ipa_logdir, tag), label='IPA')
+# 
+#     ax.set_xlabel('Training step', fontsize=fontsize)
+#     ylabel = ' '.join(tag.split('.')).capitalize()
+#     ax.set_ylabel(ylabel, fontsize=fontsize)
+#     ax.set_yscale('log')
+#     ax.legend()
+# 
+#     fig_filename = 'tacotron2_' + tag.replace('.','_') + '.png'
+#     # plt.show(fig)
+#     fig.savefig(os.path.join(fig_dir, fig_filename))
